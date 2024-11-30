f22b23d18fa60332bda1e0c5b07e96024e42a18d
Christian Rosdahl <christian.rosdahl@gmail.com>
2024-11-30 17:18:01 +0100

    Change year to 2024

--- day01.py
+++ day01.py
@@ -1,16 +1,20 @@
-# Advent of Code 2023 - day 1
+# Advent of Code 2024 - day 1
 
 # %% Imports
 from common import get_input
 
+
 # %% Part 1
 def main():
-    input_data = get_input('ex_input01.txt')
+    input_data = get_input("ex_input01.txt")
+
 
 main()
 
+
 # %% Part 2
 def main2():
-    input_data = get_input('ex_input01.txt')
+    input_data = get_input("ex_input01.txt")
+
 
 main2()
--- template.py
+++ template.py
@@ -1,16 +1,20 @@
-# Advent of Code 2023 - day D
+# Advent of Code 2024 - day D
 
 # %% Imports
 from common import get_input
 
+
 # %% Part 1
 def main():
-    input_data = get_input('ex_inputDD.txt')
+    input_data = get_input("ex_inputDD.txt")
+
 
 main()
 
+
 # %% Part 2
 def main2():
-    input_data = get_input('ex_inputDD.txt')
+    input_data = get_input("ex_inputDD.txt")
+
 
 main2()
